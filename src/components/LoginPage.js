0247b9789319de9c3bf20af174e03280d6a7cd94
Cameron Moore <cmrnmr@gmail.com>
2020-06-16 14:05:17 -0400

    started loginpage

---
+++ src/components/LoginPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useForm } from 'react';
+
+export const LoginPage = () => {
+  
+  // const {handleSubmit, register, errors} = useForm();
+
+  return(
+    <form>
+      <label>username</label>
+      <input/>
+
+      <label>password</label>
+      <input/>
+
+    </form>
+  )
+
+}
+
+
+
