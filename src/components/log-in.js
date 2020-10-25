a0b059d5421fb7874cf17e1bab512b419b599fe7
Cameron Moore <cmrnmr@gmail.com>
2020-10-25 11:22:07 -0400

    fleshing out first component: LogIn

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -3,7 +3,15 @@ import React from 'react';
 const LogIn = () => {
   return (
     <>
+      <h1>Log In to Secret Family Recipes</h1>
+      <form>
 
+        <input/>
+
+        <input/>
+
+        <button type="submit" >Go</button>
+      </form>
     </>
   )
 }
