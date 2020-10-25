7319853c3decc8f13cf119a2e3521a8b0fd705b0
Cameron Moore <cmrnmr@gmail.com>
2020-10-25 12:09:01 -0400

    finished structural outline for LogIn

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,6 +5,11 @@ import CreateRecipe from '../components/create-recipe';
 const Dashboard  = () => {
   return (
     <>
+    <h1>Dashboard</h1>
+      <ul>
+        <li>RecipeCollection</li>
+        <li>CreateRecipe</li>
+      </ul>
     </>
   )
 }
