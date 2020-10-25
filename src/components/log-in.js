99ef220e913c31af228a5451361c54db18063693
Cameron Moore <cmrnmr@gmail.com>
2020-10-25 16:38:09 -0400

    basic form sets input to state

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -4,17 +4,21 @@ import React, { useState } from 'react';
 const LogIn = () => {
 
   //an object in state equal to 'credentials' that will hold the value of the inputs in a preferable format
-  const [credentials, setCredentials] = useState({username: " ", password: " "})
+  const [credentials, setCredentials] = useState({username: "", password: ""})
 
-  //this triggers when we click to submit
+  //this triggers when we click submit
   const handleSubmit = (e) => {
   //stop default behavior that the click event triggers on the form element
   }
 
-  //this triggers when any changes to the value of the cooresponding element occurs
+  //this triggers when any changes to the value of input element occurs
   const handleChanges = (e) => {
   //spreading and setting
-  //spread any occurring value changes of event target into our setter hook
+    setCredentials({...credentials,
+      [e.target.name]: e.target.value
+  })
+  console.log(credentials)
+  //spread any occurring value changes of event targets into corresponding props in state
   }
 
   return (
