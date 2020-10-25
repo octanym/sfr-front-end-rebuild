13d17c755ec2662066a47d2945396d7d03aedf82
Cameron Moore <cmrnmr@gmail.com>
2020-10-25 16:53:40 -0400

    getting 401 back from be

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
-
+import { axiosWithAuth } from '../utils/axios-auth';
+import { useHistory } from 'react-router-dom';
 
 const LogIn = () => {
 
   //an object in state equal to 'credentials' that will hold the value of the inputs in a preferable format
-  const [credentials, setCredentials] = useState({username: "", password: ""})
+  const [credentials, setCredentials] = useState({user_name: "", password: ""})
+
+  //provide access to the history prop in react-router-dom via hooks
+  const history = useHistory()
 
   //this triggers when we click submit
   const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/auth/login', credentials)
+      .then( res => {
+        window.localStorage.setItem('token', res.data.payload)
+      })
+      .catch( err => console.log( 'there was an issue retrieving data:', err ))
   //stop default behavior that the click event triggers on the form element
   }
 
@@ -15,10 +26,10 @@ const LogIn = () => {
   const handleChanges = (e) => {
   //spreading and setting
     setCredentials({...credentials,
+      //spread any occurring value changes of event targets into corresponding props in the object state
       [e.target.name]: e.target.value
-  })
-  console.log(credentials)
-  //spread any occurring value changes of event targets into corresponding props in state
+    })
+    console.log(credentials)
   }
 
   return (
