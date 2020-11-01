75c54738d08bce54d4a38c50b46ae49eb0baeaa9
Cameron Moore <cmrnmr@gmail.com>
2020-11-01 11:44:09 -0500

    built out recipe card recipe details and removed package lock json and yanr lock and reran yarn isntall

--- src/components/recipe-details.js
+++ src/components/recipe-details.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import RecipeCard from '../components/recipe-card';
 
-const RecipeDetails = () => {
+const RecipeDetails = (props) => {
   return (
-    <>
-
-    </>
+    <Link to="/recipes/{props.recipe.id}">
+      <RecipeCard recipe={props.recipe}/>
+    </Link>
   )
 }
 
--- src/components/recipes-collection.js
+++ src/components/recipes-collection.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axios-auth';
 import RecipeDetails from '../components/recipe-details'
 
+const id = () => {
+  return JSON.parse(window.localStorage.getItem('users_id'))
+}
+
 const RecipeCollection = () => {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get(`/auth/${id()}/recipes`)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <>
+      {recipes.map(recipe => (
+        <RecipeDetails/>
+      ))}
     </>
   )
 }
